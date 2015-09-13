7796dc921b7b7b395123c0e2cb051693e7481b27
Alexander Breen <alexander.breen@gmail.com>
2015-09-13 17:57:11 -0400

    Fixed bug in depts loader

--- add_depts.py
+++ add_depts.py
@@ -3,6 +3,7 @@ from coursedb.models import School, Department
 
 if len(sys.argv) != 3:
     print('usage: {} school_symbol departments_tsv_path'.format(sys.argv[0]))
+    sys.exit(1)
 
 school_symbol = sys.argv[1]
 path = sys.argv[2]
@@ -11,7 +12,11 @@ school = School.objects.get(symbol=school_symbol)
 with open(path, 'r') as f:
     for dept in f:
         symbol, name = dept.split('\t')
+
+        symbol = symbol.strip()
+        name = name.strip()
+
         d = Department(school=school, symbol=symbol, name=name)
         d.save()
 
-        print('added ' + str(d))
+        print('added ' + name + ' (' + symbol + ') to school ' + school)
