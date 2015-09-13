3075ec1cd946491d1963384235345f64551f8b22
Alexander Breen <alexander.breen@gmail.com>
2015-09-13 18:41:34 -0400

    Setup script now stops with errors

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+if [[ -e db.sqlite3 ]]; then
+    echo error: database already present
+    exit 1
+fi
+
 # use Django tools to set up empty database
-python3 manage.py migrate
+python3 manage.py migrate || exit 2
 
 # add all schools
-python3 add_schools.py rawdata/schools.tsv
+python3 add_schools.py rawdata/schools.tsv || exit 3
 
 # add departments
-python3 add_depts.py CAS rawdata/cas/depts.tsv
-python3 add_depts.py CFA rawdata/cfa/depts.tsv
+python3 add_depts.py CAS rawdata/cas/depts.tsv || exit 4
+python3 add_depts.py CFA rawdata/cfa/depts.tsv || exit 4
+
+# add courses
+#python3 add_courses.py rawdata/cas/courses.tsv || exit 5
+python3 add_courses.py rawdata/cfa/courses.tsv || exit 5
