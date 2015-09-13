10edf6562e05ad9dd4e9918557c3f96800d53da9
Alexander Breen <alexander.breen@gmail.com>
2015-09-13 18:21:24 -0400

    Updated schools list

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ python3 manage.py migrate
 # add all schools
 python3 add_schools.py rawdata/schools.tsv
 
-# add CAS departments
+# add departments
 python3 add_depts.py CAS rawdata/cas/depts.tsv
+python3 add_depts.py CFA rawdata/cfa/depts.tsv
