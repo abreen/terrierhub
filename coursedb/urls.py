d6bbfc7c37b22e13c36f6588bbb149af99ee7401
sarahpmedeiros <sarahpmedeiros@gmail.com>
2015-12-05 13:53:04 -0500

    first version of schedule

--- coursedb/urls.py
+++ coursedb/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/(?P<function>.+)$', views.api, name='api'),
+    url(r'^schedule$', views.schedule, name='schedule'),
     url(r'^(?P<school>[^/]+)/$', views.school, name='school'),
     #url(r'^(?P<school>.+)/(?P<dept>.+)/$', views.department, name='department'),
     url(r'^(?P<school>.+)/(?P<dept>.+)/(?P<num>[0-9]+)$', views.course, name='course'),
