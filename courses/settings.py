8c997f58a0a3f0bebd1e57b60281bba3bf22d55c
John Gonsalves <johnnyg7@bu.edu>
2015-12-06 13:51:59 -0500

    updated debug

--- courses/settings.py
+++ courses/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y0h$9(%%mo^m!y!3ax&ymyq)2(^(=zyg%5rmwfll4g7dicx&!-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 ALLOWED_HOSTS = ["*"]
 #ALLOWED_HOSTS =['localhost', '8000']
 #TEMPLATE_DEBUG = DEBUG
